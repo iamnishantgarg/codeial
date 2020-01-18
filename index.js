a2e3c094ea415f8395dc8a22ce7c063679f3e9f0
Nishant garg <iamnishantgarg@gmail.com>
2020-01-18 16:45:57 +0530

    db added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const route = require("./routes/index");
 const PORT = 8000;
+const keys = require("./keys");
+const mongoose = require("mongoose");
 const expressLayout = require("express-ejs-layouts");
 
 app.use(express.static("./assets"));
@@ -12,7 +14,14 @@ app.use(expressLayout);
 
 app.set("view engine", "ejs");
 app.use("/", route);
-app.listen(PORT, err => {
+mongoose.connect(keys.MONGOURI, { useNewUrlParser: true }, err => {
   if (err) console.log(err);
-  else console.log(`server is running ${PORT}`);
+  else {
+    console.log("connected to db");
+
+    app.listen(PORT, err => {
+      if (err) console.log(err);
+      else console.log(`server is running ${PORT}`);
+    });
+  }
 });
