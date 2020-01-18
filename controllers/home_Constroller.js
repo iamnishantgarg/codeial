9685c4a78f043aed64c7201c4f945f3a83ef1cbe
Nishant garg <iamnishantgarg@gmail.com>
2020-01-18 16:37:03 +0530

    layout added

--- controllers/home_Constroller.js
+++ controllers/home_Constroller.js
@@ -1,3 +1,4 @@
 exports.home = (req, res, next) => {
-  res.send("<h1>Express is working for codeial</h1>");
+  //   res.send("<h1>Express is working for codeial</h1>");
+  res.render("home", { title: "home" });
 };
--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const route = require("./routes/index");
 const PORT = 8000;
+const expressLayout = require("express-ejs-layouts");
+
+app.use(express.static("./assets"));
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
+app.use(expressLayout);
+
 app.set("view engine", "ejs");
 app.use("/", route);
 app.listen(PORT, err => {
